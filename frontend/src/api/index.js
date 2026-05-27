f3593eff3dca82e79cbb1222feeb2199bb950b60
Prathik A <prathikx29@gmail.com>
2026-05-27 12:39:33 +0530

    Production API URL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || "/api", timeout: 15000 });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || "/api" , timeout: 15000 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
